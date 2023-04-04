a80194c9fa7d2e3bfe0303af3390f572505620f8
neerajkumarc <chaudharyneeraj91@gmail.com>
2023-04-04 17:52:03 +0530

    initial build

---
+++ app.py
@@ -0,0 +1,43 @@
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes, MessageHandler, filters
+import json
+import random
+
+import os
+from dotenv import load_dotenv
+load_dotenv()
+TOKEN = os.getenv('TOKEN')
+
+with open('./quotes.json', 'r') as file:
+    data = json.load(file)
+
+
+async def randomquote(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    datadict =(data[random.randint(0,(len(data)-1))])
+    quote = datadict["quote"]
+    author = datadict["author"]
+    text = f"{quote}\nAuthor: {author}"
+    await update.message.reply_text(text)
+
+async def sryText(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = "Sorry I can't understand that command. Please type /help to get all the commands available."
+    await update.message.reply_text(text)
+
+async def help(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = "/help - list all the commands \n/random - get a random quote"
+    await update.message.reply_text(text)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    text = "Welcome to QuantumQuotations!\n\n Commands: \n/help - list all the commands \n/random - get a random quote\n\n\n ~Developed by Neerajkumar"
+    await update.message.reply_text(text)
+
+
+app = ApplicationBuilder().token(TOKEN).build()
+
+
+app.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), sryText))
+app.add_handler(CommandHandler("random", randomquote))
+app.add_handler(CommandHandler("help", help))
+app.add_handler(CommandHandler("start", start))
+
+app.run_polling()
